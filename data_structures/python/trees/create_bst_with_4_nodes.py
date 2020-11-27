1e1c6e202e4078fea8433bedbe2c588aa30e7abf
parzuko <jivansh17@gmail.com>
2020-11-27 21:42:35 +0530

    start bst

---
+++ data_structures/python/trees/create_bst_with_4_nodes.py
@@ -0,0 +1,19 @@
+"""
+Structure of what we're gonna create
+
+      tree
+      ----
+       1    <-- root
+     /   \
+    2     3  
+   /   
+  4
+
+"""
+
+class BinarySearchTree:
+	def __init__(self, data):
+		self.value = data
+		self.right = None
+		self.right = None
+		
