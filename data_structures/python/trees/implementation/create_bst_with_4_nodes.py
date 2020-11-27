3c5fe6cd61af95594de786d9f42931c3b3b10202
parzuko <jivansh17@gmail.com>
2020-11-27 21:50:17 +0530

    fix formatting

--- data_structures/python/trees/implementation/create_bst_with_4_nodes.py
+++ data_structures/python/trees/implementation/create_bst_with_4_nodes.py
@@ -15,34 +15,33 @@ class BinarySearchTree:
 	def __init__(self, data):
 		self.value = data
 		self.right = None
-		self.right = None
+		self.left = None
 		
 
 # create root 
-root = Node(1) 
-'''
-        1 
-      /   \ 
-     None  None'''
+root = BinarySearchTree(1) 
+    #     1 
+    #   /   \ 
+    #  None  None
   
-root.left      = Node(2); 
-root.right     = Node(3); 
+root.left      = BinarySearchTree(2)
+root.right     = BinarySearchTree(3)
     
-''' 2 and 3 become left and right children of 1 
-           1 
-         /   \ 
-        2      3 
-     /    \    /  \ 
-   None None None None'''
+# 2 and 3 become left and right children of 1 
+#            1 
+#          /   \ 
+#         2      3 
+#      /    \    /  \ 
+#    None None None None
   
   
-root.left.left  = Node(4); 
-'''4 becomes left child of 2 
-           1 
-       /       \ 
-      2          3 
-    /   \       /  \ 
-   4    None  None  None 
-  /  \ 
-None None'''
-  
+root.left.left  = BinarySearchTree(4)
+
+# 4 becomes left child of 2 
+#            1 
+#        /       \ 
+#       2          3 
+#     /   \       /  \ 
+#    4    None  None  None 
+#   /  \ 
+# None None
