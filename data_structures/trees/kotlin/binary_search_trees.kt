6b95b6bd5d87b6f07d734559daeaa5ff0fc047be
parzuko <jivansh17@gmail.com>
2020-11-30 17:23:23 +0530

    kotlin BST implementation

--- data_structures/trees/kotlin/binary_search_trees.kt
+++ data_structures/trees/kotlin/binary_search_trees.kt
@@ -6,6 +6,25 @@ kotlinc binary_search_trees.kt -include-runtime -d binary_search_trees.jar
 java -jar binary_search_trees.jar
  */
 
+class BinarySearchTreeNode(
+    val value: Int,
+    var left: BinarySearchTreeNode? = null,
+    var right: BinarySearchTreeNode? = null,
+)
+
+
 fun main() {
-    println("Hello World")
+    val root = BinarySearchTreeNode(1)
+    val two = BinarySearchTreeNode(2)
+    val three = BinarySearchTreeNode(3)
+    val four = BinarySearchTreeNode(4)
+    val five = BinarySearchTreeNode(5)
+
+    root.left = two
+    root.right = three
+    two.left = four
+    two.right = five
+
+    println("Created BST!")
+
 }
