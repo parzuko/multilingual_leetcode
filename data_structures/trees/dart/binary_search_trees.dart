82633f12f88d5ce1acf8c654bfeac86fc0f7eeef
parzuko <jivansh17@gmail.com>
2020-11-30 16:55:45 +0530

    dartlang bst implementation

--- data_structures/trees/dart/binary_search_trees.dart
+++ data_structures/trees/dart/binary_search_trees.dart
@@ -1,10 +1,17 @@
 class BinarySearchTreeNode{
-  int value;
+  final int value;
   BinarySearchTreeNode left;
   BinarySearchTreeNode right;
+
+  BinarySearchTreeNode(this.value, this.left, this.right);
 }
 
 
 void main(){
-  
+  BinarySearchTreeNode root = BinarySearchTreeNode(1,null,null);
+  root.left = BinarySearchTreeNode(2,null,null);
+  root.right = BinarySearchTreeNode(3,null,null);
+  root.left.left = BinarySearchTreeNode(4,null,null);
+  root.left.right = BinarySearchTreeNode(5,null,null);
+  print("Created BST!");
 }
