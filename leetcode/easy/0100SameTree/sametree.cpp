4752c6aca1f2205dab4e1b288747eaa888425354
parzuko <jivansh17@gmail.com>
2020-12-03 13:51:36 +0530

    0100 cpp iterative

---
+++ leetcode/easy/0100SameTree/sametree.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class IterativeSolution
+{
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q)
+    {
+
+        stack<TreeNode *> stack1, stack2;
+        stack1.push(p), stack2.push(q);
+        while (!stack1.empty() || !stack2.empty())
+        {
+            TreeNode *cur1 = stack1.top();
+            stack1.pop();
+
+            TreeNode *cur2 = stack2.top();
+            stack2.pop();
+
+            if (cur1 && !cur2)
+                return false;
+            if (!cur1 && cur2)
+                return false;
+            if (!cur1 && !cur2)
+                continue;
+
+            if (cur1->val != cur2->val)
+                return false;
+            stack1.push(cur1->right);
+            stack1.push(cur1->left);
+            stack2.push(cur2->right);
+            stack2.push(cur2->left);
+        }
+
+        return stack1.empty() && stack2.empty();
+    }
+};
