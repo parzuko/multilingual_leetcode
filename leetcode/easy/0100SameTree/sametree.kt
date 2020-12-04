49cb20a4105bbdbff2ae61b3164bfcd3ee021787
parzuko <jivansh17@gmail.com>
2020-12-04 08:33:30 +0530

    0100 kotlin iterative

--- leetcode/easy/0100SameTree/sametree.kt
+++ leetcode/easy/0100SameTree/sametree.kt
@@ -4,9 +4,44 @@
  * var v = ti.`val`
  */
 
-class TreeNode(var `val`: Int) {
+data class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+}
+
+class IterativeSolution{
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        val stk1 = Stack<TreeNode>()
+        val stk2 = Stack<TreeNode>()
+        stk1.push(p)
+        stk2.push(q)
+
+        while (stk1.isNotEmpty() || stk2.isNotEmpty()){
+            var cur1 = stk1.peek()
+            stk1.pop()
+            var cur2 = stk2.peek()
+            stk2.pop()
+
+            if (cur1 is TreeNode && cur2 !is TreeNode){
+                return false
+            }
+            if (cur1 !is TreeNode && cur2 is TreeNode){
+                return false
+            }
+            if (cur1 !is TreeNode && cur2 !is TreeNode){
+                continue
+            }
+
+            if (cur1.`val` != cur2.`val`){
+                return false
+            }
+            stk1.push(cur1.right)
+            stk1.push(cur1.left)
+            stk2.push(cur2.right)
+            stk2.push(cur2.left)
+        }
+        return !stk1.isNotEmpty() && !stk1.isNotEmpty()
+    }
 }
 
 class ReccursiveSolution {
