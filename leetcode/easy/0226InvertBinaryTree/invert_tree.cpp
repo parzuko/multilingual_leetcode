6f1c0f5516e4f19e454a946e26d5d7a30e5669bc
parzuko <jivansh17@gmail.com>
2020-12-04 20:17:11 +0530

    0266 cpp iterative

--- leetcode/easy/0226InvertBinaryTree/invert_tree.cpp
+++ leetcode/easy/0226InvertBinaryTree/invert_tree.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "bits/stdc++.h";
 using namespace std;
 struct TreeNode
 {
@@ -9,6 +9,32 @@ struct TreeNode
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
+
+class IterativeSolution
+{
+public:
+    TreeNode *invertTree(TreeNode *root)
+    {
+        if (root == nullptr)
+            return nullptr;
+        queue<TreeNode*> Queue;
+        Queue.push(root);
+        while (!Queue.empty())
+        {
+            TreeNode *current = Queue.front();
+            Queue.pop();
+            TreeNode *temp = current->left;
+            current->left = current->right;
+            current->right = temp;
+            if (current->left != nullptr)
+                Queue.push(current->left);
+            if (current->right != nullptr)
+                Queue.push(current->right);
+        }
+        return root;
+    }
+};
+
 class RecursiveSolution
 {
 public:
