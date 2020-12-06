4bcff702cc1782e4319c069d924bd1e4b0b61ad7
parzuko <jivansh17@gmail.com>
2020-12-06 19:23:27 +0530

    0104 kotlin iterative

--- leetcode/easy/0104MaxDepth/depth.kt
+++ leetcode/easy/0104MaxDepth/depth.kt
@@ -3,6 +3,39 @@ data class TreeNode(var `val`: Int) {
     var right: TreeNode? = null
 }
 
+class IterativeSolution{
+    fun maxDepth(root: TreeNode?): Int {
+        if(root == null){
+            return 0
+        }
+        val queue: Queue<TreeNode> = LinkedList<TreeNode>()
+        queue.add(root)
+        var maxHeight = 0
+
+        while (true){
+            var numberOfNodes = queue.size
+            if (numberOfNodes == 0){
+                return numberOfNodes
+            }
+            numberOfNodes++
+
+            while(numberOfNodes > 0){
+                val current = queue.peek()
+                queue.remove()
+                if (current.left != null){
+                    queue.add(current.left)
+                }
+                if (current.right != null){
+                    queue.add(current.right)
+                }
+                numberOfNodes--;
+            }
+        }
+    }
+}
+
+
+
 class RecursiveSolution {
     fun maxDepth(root: TreeNode?): Int {
         if(root == null){
