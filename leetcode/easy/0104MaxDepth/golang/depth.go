d68eba9a6dcd133a93cce72028ae677545e608c5
parzuko <jivansh17@gmail.com>
2020-12-06 19:35:03 +0530

    0104 golang iterative

--- leetcode/easy/0104MaxDepth/golang/depth.go
+++ leetcode/easy/0104MaxDepth/golang/depth.go
@@ -7,6 +7,36 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+func maxDepthIterative(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	maxHeight := 0
+	for 1 == 1 {
+		numberOfNodes := len(queue)
+		if numberOfNodes == 0 {
+			return maxHeight
+		}
+		maxHeight++
+
+		for numberOfNodes > 0 {
+			current := queue[0]
+			queue = queue[1:]
+			if current.Left != nil {
+				queue = append(queue, current.Left)
+			}
+			if current.Right != nil {
+				queue = append(queue, current.Right)
+			}
+			numberOfNodes--
+		}
+	}
+	return 0
+
+}
+
 func maxDepthRecursive(root *TreeNode) int {
 	if root == nil {
 		return 0
