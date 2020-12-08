f4c8a2a51751d0d04f8c9cad07aac3758fc77ef7
parzuko <jivansh17@gmail.com>
2020-12-08 14:31:19 +0530

    0098 iterative python

--- leetcode/medium/0098ValidateBST/valid.py
+++ leetcode/medium/0098ValidateBST/valid.py
@@ -4,7 +4,30 @@ class TreeNode(object):
         self.val = val
         self.left = left
         self.right = right
-class Solution:
+
+class IterativeSolution:
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+            
+        stack = [(root, float('-inf'), float('inf')), ] 
+        while stack:
+            root, lower, upper = stack.pop()
+            if not root:
+                continue
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+            stack.append((root.right, val, upper))
+            stack.append((root.left, lower, val))
+        return True  
+
+
+class RecursiveSolution:
     def isValidBST(self, root):
         """
         :type root: TreeNode
