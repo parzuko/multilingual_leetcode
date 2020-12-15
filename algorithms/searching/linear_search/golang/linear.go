b1a3a25dd22b228b28c3abbf8134e1b7b812e1ce
parzuko <jivansh17@gmail.com>
2020-12-15 21:59:23 +0530

    linear search in golang

--- algorithms/searching/linear_search/golang/linear.go
+++ algorithms/searching/linear_search/golang/linear.go
@@ -1,5 +1,26 @@
-package golang
+package main
+
+import "fmt"
+
+func linearSearch(numbers []int, target int) int {
+	index := 0
+	for _, item := range numbers {
+		if item == target {
+			return index
+		}
+		index++
+	}
+	return -1
+}
 
 func main() {
+	numbers := []int{2, 3, 4, 10, 20}
+	target := 10
+	result := linearSearch(numbers, target)
+	if result == -1 {
+		fmt.Println("Couldn't find target :(")
+	} else {
+		fmt.Println("Found target at index:", result)
+	}
 
 }
