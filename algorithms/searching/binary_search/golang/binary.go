f1299efdff5976a1799e0c020f9a5149a1047ac9
parzuko <jivansh17@gmail.com>
2020-12-16 20:26:57 +0530

    recursive binary search golang

--- algorithms/searching/binary_search/golang/binary.go
+++ algorithms/searching/binary_search/golang/binary.go
@@ -5,12 +5,33 @@ import "fmt"
 func main() {
 	numbers := []int{2, 3, 4, 10, 23, 140}
 	target := 10
-	result := iterativeBinarySearch(numbers, target)
-
-	if result == -1 {
+	// iterative method
+	iterativeResult := iterativeBinarySearch(numbers, target)
+	if iterativeResult == -1 {
 		fmt.Println("Couldn't find", target, ":(")
 	} else {
-		fmt.Println("Found", target, "at index", result, "!")
+		fmt.Println("Found", target, "at index", iterativeResult, "!")
+	}
+	// recursive method
+	recusiveResult := recursiveBinarySearch(numbers, target, 0, len(numbers)-1)
+	if recusiveResult == -1 {
+		fmt.Println("Couldn't find", target, ":(")
+	} else {
+		fmt.Println("Found", target, "at index", recusiveResult, "!")
+	}
+}
+
+func recursiveBinarySearch(array []int, target int, lowIndex int, highIndex int) int {
+	if highIndex < lowIndex {
+		return -1
+	}
+	mid := int((lowIndex + highIndex) / 2)
+	if array[mid] > target {
+		return recursiveBinarySearch(array, target, lowIndex, mid)
+	} else if array[mid] < target {
+		return recursiveBinarySearch(array, target, mid+1, highIndex)
+	} else {
+		return mid
 	}
 }
 
