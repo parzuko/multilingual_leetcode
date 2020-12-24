f3b84da75f266dd83ba3f4cf56ee31d413a6fe60
parzuko <jivansh17@gmail.com>
2020-12-24 16:53:12 +0530

    golang recursive 700

---
+++ leetcode/easy/0700SearchBST/golang/search.go
@@ -0,0 +1,22 @@
+package golang
+
+// TreeNode Data Class
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val == val {
+		return root
+	}
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	}
+	return searchBST(root.Right, val)
+
+}
