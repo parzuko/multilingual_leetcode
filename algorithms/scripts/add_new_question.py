442c9e7d147b90b926f5fc15a01cec9242660cf5
parzuko <jivansh17@gmail.com>
2021-01-07 23:43:39 +0530

    finish readme automater

--- algorithms/scripts/add_new_question.py
+++ algorithms/scripts/add_new_question.py
@@ -1,16 +1,19 @@
-'''
- [{question_number}](link_to_question) | [question_statement](leetcode/{difficulty}.lower()/{folder_name})| {difficulty}| [Python](leetcode/{diff}/{folder}/{file_name}.py), [C++](leetcode/{diff}/{folder}/{file_name}.cpp), [Golang](leetcode/{diff}/{folder}/{file_name}.go), [Kotlin](leetcode/{diff}/{folder}/{file_name}.kt)| 
-'''
-
-
 def make_response():
     output = ""
-
+    question_number = input("Enter the question number: ")
+    link_to_question = input("Enter the question link: ")
+    question_statement = input("Enter the question statement: ")
+    diff = input("Enter question difficulty: ")
+    folder = input("Enter folder name: ")
+    difficulty = diff.capitalize()
+    file_name = input("Enter file name: ")
+    output = f"[{question_number}]({link_to_question}) | [{question_statement}](leetcode/{diff}/{folder})| {difficulty}| [Python](leetcode/{diff}/{folder}/{file_name}.py), [C++](leetcode/{diff}/{folder}/{file_name}.cpp), [Golang](leetcode/{diff}/{folder}/{file_name}.go), [Kotlin](leetcode/{diff}/{folder}/{file_name}.kt)|"
     return output
 
 
 def main():
     answer = make_response()
+    print("Here you go!")
     print(answer)
 
 if __name__ == "__main__":
