ba92100bc3e62014a08b7197d22c558988ae492e
parzuko <jivansh17@gmail.com>
2021-01-22 22:36:20 +0530

    add kth min max cpp

---
+++ data_structures/arrays/003KthMinMax/kth.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct KthMinMax {
+  int min;
+  int max;
+};
+
+struct KthMinMax find_kth(int array[], int k, int len) {
+  int kth_min = array[k - 1];
+  int kth_max = array[len - k];
+  KthMinMax min_max;
+  min_max.min = kth_min;
+  min_max.max = kth_max;
+  return min_max;
+}
+
+int main() {
+  // code
+  int array[] = {141, 51, 551, 63, 422, 15156, 221, 2, 4};
+  int len = sizeof(array) / sizeof(array[0]);
+
+  // will eventually learn quick/merge sort so this is ok
+  sort(array, array + len);
+  int kth = 2;  // make this cin potentially
+
+  KthMinMax kth_min_max = find_kth(array, kth, len);
+  cout << "The Kth Min Is: " << kth_min_max.min << endl;
+  cout << "The Kth Max Is: " << kth_min_max.max << endl;
+}
+
+/*
+
+1,2,4,5,6,7,8,9
+
+if k = 1
+len = 9
+
+max = len - k - 1
+min = k - 1
+*/
---
+++ data_structures/arrays/003KthMinMax/readme.md
@@ -0,0 +1,10 @@
+Examples : 
+ 
+
+    Input  : arr[] = {1241, 1152, 5423, 14, 1}
+             k = 1
+    Output : arr[] = {1, 5423}
+
+    Input :  arr[] = {4, 5, 1, 2}
+             k = 2
+    Output : arr[] = {2, 4}
