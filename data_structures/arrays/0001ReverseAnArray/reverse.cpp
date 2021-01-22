d4755111b0ded8c624d57a76f37f5cb032fd6df0
parzuko <jivansh17@gmail.com>
2021-01-22 12:23:56 +0530

    finish q1 cpp

--- data_structures/arrays/0001ReverseAnArray/reverse.cpp
+++ data_structures/arrays/0001ReverseAnArray/reverse.cpp
@@ -22,10 +22,20 @@ void reverse_array(int array[], int start_index, int end_index) {
   }
 }
 
+void reverse_recursively(int array[], int start_index, int end_index) {
+  if (start_index >= end_index) return;
+  swap_numbers(&array[start_index], &array[end_index]);
+  reverse_recursively(array, start_index + 1, end_index - 1);
+}
+
 int main() {
   int array[] = {1, 2, 3, 4, 5};
   int start_index = 0;
   int end_index = sizeof(array) / sizeof(array[0]) - 1;
   reverse_array(array, start_index, end_index);
+  cout << "Reverse Array Is:\n";
+  print_array(array, end_index + 1);
+  reverse_recursively(array, start_index, end_index);
+  cout << "Rearranged Array Is:\n";
   print_array(array, end_index + 1);
 }
