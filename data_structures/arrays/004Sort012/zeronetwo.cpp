306f01bcc31a93982ca5ebb762fa4e5fe7ee171b
parzuko <jivansh17@gmail.com>
2021-01-24 16:42:30 +0530

    add dutch national flag version for zeronetwo

--- data_structures/arrays/004Sort012/zeronetwo.cpp
+++ data_structures/arrays/004Sort012/zeronetwo.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // O(n) time O(1) space but not optimal
 void count_zero_one_two(int array[], int len) {
   int zero = 0;
@@ -30,11 +29,31 @@ void count_zero_one_two(int array[], int len) {
   }
 }
 
+void dutch_national_flag(int array[], int len) {
+  int low = 0;
+  int high = len - 1;
+  int mid = 0;
+
+  while (mid <= high) {
+    switch (array[mid]) {
+      case 0:
+        swap(array[low++], array[mid++]);
+        break;
+      case 1:
+        mid++;
+        break;
+      case 2:
+        swap(array[high--], array[mid]);
+    }
+  }
+}
+
 int main() {
   // codes
   int array[] = {1, 0, 2, 1, 0, 0, 0, 1, 2, 2, 2, 1, 0};
   int len = sizeof(array) / sizeof(array[0]);
-  count_zero_one_two(array, len);
+  // count_zero_one_two(array, len);
+  dutch_national_flag(array, len);
   for (int i = 0; i < len; i++) {
     cout << array[i] << " ";
   }
