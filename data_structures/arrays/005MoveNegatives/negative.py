bb9bdec334dcb7439023e7c08e7f5e589110e5b0
parzuko <jivansh17@gmail.com>
2021-01-24 22:22:56 +0530

    move negatives python

---
+++ data_structures/arrays/005MoveNegatives/negative.py
@@ -0,0 +1,15 @@
+def move_negatives(array: list) -> None:
+    size = len(array)
+    j = 0
+    for i in range(size):
+        if array[i] < 0 :
+            array[j], array[i] = array[i], array[j]
+            j += 1
+def main():
+    array = [1,1,24,51,51,215,125,6451,-12,-89]
+    move_negatives(array)
+    print("Modified Array Is:")
+    print(array)
+
+if __name__ == "__main__":
+    main()
