ae550a349377d7d4c0f65ab146eaf986916e051c
parzuko <jivansh17@gmail.com>
2021-02-14 22:10:08 +0530

    add golang remove nth node

---
+++ algo_expert/medium/022RemoveNthNodeFromEnd/remove.go
@@ -0,0 +1,27 @@
+package RemoveNthNodeFromEnd
+
+// LinkedList Data Class
+type LinkedList struct {
+	Value int
+	Next  *LinkedList
+}
+
+//RemoveNthNodeFromEnd implementation
+func RemoveNthNodeFromEnd(head *LinkedList, nth int) {
+	counter, first, second := 1, head, head
+
+	for counter <= nth {
+		second = second.Next
+		counter ++
+	}
+
+	if second == nil {
+		head.Value = head.Next.Value
+		head.Next = head.Next.Next
+	}
+	for second.Next != nil {
+		second = second.Next
+		first = first.Next
+	}
+	first.Next = first.Next.Next
+}
