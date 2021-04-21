85a717cc8ec041cab00a54b357fabbfba8f28202
parzuko <jivansh17@gmail.com>
2021-04-22 00:26:59 +0530

    create template

---
+++ competitive_programming/codeforces/div2/717/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define watch(x) cout << (#x) << " " << (x)
+#define PI double(2 * acos(0.0))
+#define LL long long
+#define MOD 100000007
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  return 0;
+}
