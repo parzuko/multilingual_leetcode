f2f816315eafb9fcc49fd1c302e1d733afdccecd
parzuko <jivansh17@gmail.com>
2021-05-10 20:00:17 +0530

    palindrome

---
+++ miscellaneous/patterns/advanced/palindrome.cpp
@@ -0,0 +1,27 @@
+// 1
+// 0 1
+// 0 1 0
+// 1 0 1 0
+// 1 0 1 0 1
+// This is the pattern
+
+#include <iostream>
+using namespace std;
+#define loop(i, n) for (int i = 0; i < n; i++)
+
+int main() {
+  int n;
+  cin >> n;
+  loop(i, n) {
+    loop(j, n - i - 1) { cout << " "; }
+    for (int j = i + 1; j >= 1; j--) {
+      if (i + 1 == 1) {
+        break;
+      }
+      cout << j;
+    }
+    loop(j, i + 1) { cout << j + 1; }
+
+    cout << "\n";
+  }
+}
