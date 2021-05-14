a0bf88481952f2f59ce91e09d40afe3b105c60ae
parzuko <jivansh17@gmail.com>
2021-05-15 01:28:43 +0530

    arrays

---
+++ miscellaneous/stl/array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[4] = {1,2,3,4};
+    int arr[4];
+    int ar[] = {12,3,12,41,5,5,16,16};
+
+    int length_of_arr = sizeof(ar)/sizeof(ar[0]);
+    
+    
+}
