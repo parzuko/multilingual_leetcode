2b3389f74a49f6a370119f69507a94ed60e85421
parzuko <jivansh17@gmail.com>
2021-05-20 02:50:55 +0530

    modularize selection

--- algorithms/sorting/selection_sort/selection.cpp
+++ algorithms/sorting/selection_sort/selection.cpp
@@ -1,33 +1,39 @@
 #include <iostream>
 using namespace std;
 
+void swap(int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void selection_sort(int unsorted_arr[], int length){
+    for(int i = 0; i < length - 1; i++){
+        for(int j = i + 1; j < length; j++){
+            if (unsorted_arr[j] < unsorted_arr[i]) 
+                swap(&unsorted_arr[i], &unsorted_arr[j]);
+        }
+    }
+}
+
+void print_array(int arr[], int length){
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
 
 int main() {
     
-    int unsorted_arr[] = {15155, 124, 9824, 12, 12, 5};
-    int length = sizeof(unsorted_arr) / sizeof(unsorted_arr[0]);
+    int arr[] = {15155, 124, 9824, 142, 12, 5};
+    int length = sizeof(arr) / sizeof(arr[0]);
     
     cout << "UnSorted Arry Is: \n";
-    for (int i =0; i < length; i++) {
-        cout << unsorted_arr[i] << " ";
-    }
-    cout << "\n";
+    print_array(arr, length);
     
+    selection_sort(arr, length);
     
-    
-    // selection sort
-    for (int i = 0; i < length - 1; i++) {
-        for (int j = i + 1; j < length; j++){
-            if (unsorted_arr[j] < unsorted_arr[i]) {
-                int temp = unsorted_arr[j];
-                unsorted_arr[j] = unsorted_arr[i];
-                unsorted_arr[i] = temp;
-            }
-        }
-    }
     cout << "Now Sorted Arry Is: \n";
-    for (int i =0; i < length; i++) {
-        cout << unsorted_arr[i] << " ";
-    }
-    cout << "\n";
+    print_array(arr, length);
 }
